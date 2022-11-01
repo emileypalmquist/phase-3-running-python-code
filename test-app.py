bdd39e284938aafa14c39daa38fa6ba97802ecc1
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-11-01 12:35:26 -0700

    test passing

---
+++ test-app.py
@@ -0,0 +1,10 @@
+# comments in python
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+# python optional end parameter to print
+print("Hello world!", end=" ")
+print("hello sun!", end="!! ")
+print("hello sky!", end="!!!\n")
+# \n creates new line
+print("hi")
